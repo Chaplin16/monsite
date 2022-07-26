380b8b53f3bec404bf61c2e8875903638666d89b
Chaplin16 <chaplin16@orange.fr>
2022-07-26 03:00:30 +0200

    rajout de cards dans le portfolio

--- js/website.js
+++ js/website.js
@@ -1,75 +1,75 @@
-const website = [
+const website = [ 
     {
         id:1,
-        title:"Les Fashion du Sud",
-        attachment:"image/website/creation-site-aigues-mortes-fashion-du-sud.png",
-        object:"Boutique fashion de vêtements pour femme",
-        link:"https://lesfashiondusud.fr",
-        code:"Développé avec Word Press",
-        alt:"creation site internet rapide petits budgets pour les fashion du sud à Aigues-Mortes."
+        title:"Mon site de developpeur",
+        attachment:"image/website/creation-site-aigues-mortes-developpeur-aurore-react.png",
+        object:"Aurore, Développeur Web ✨",
+        link:"https://aurore-ramos-cv.vercel.app/",
+        code: "Codé avec React et Javascript",
+        alt:"creation site internet rapide petits budgets pour le cordonnier à Aigues Mortes"
     },
     {
         id:2,
-        title:"Angélina Bike",
-        attachment:"image/website/creation-site-aigues-mortes-angelina-bike.png",
-        object:"Boutique de location et d'achat de vélo électrique et moto",
-        link:"https://angelina-bike.fr",
-        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets  pour Angélina à la Grande Motte."
+        title:"Cordonnerie des Remparts",
+        attachment:"image/website/creation-site-internet-aigues-mortes-cordonnerie-remparts.png",
+        object:"Cordonnerie sur Aigues-Mortes",
+        link:"https://chaplin16.github.io/cordonnerie/",
+        code: "Codé en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour le cordonnier à Aigues Mortes"
     },
     {
         id:3,
-        title:"David BARRY",
-        attachment:"image/website/creation-site-aigues-mortes-cordonnerie-david-barry.png",
-        object:"Site pour un maçon",
-        link:"https://david-barry.fr",
-        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour le site du maçon d Aigues Mortes"
+        title:"Jpcapsules",
+        attachment:"image/website/creation-site-aigues-mortes-jpcapsules.png",
+        object:"Site de ventes de capsules de champagne",
+        link:"https://jpcapsules.fr",
+        code: "Développé avec Prestashop",
+        alt:"creation site internet rapide petits budgets pour jpcapsules à Aigues Mortes"
     },
     {
         id:4,
-        title:"Salon 46",
-        attachment:"image/website/creation-site-aigues-mortes-salon-46.png",
-        object:"Salon de coiffure sur Aigues-Mortes",
-        link:"https://salon-46.fr",
-        code:"Développé avec Word Press",
-        alt:"creation site internet rapide petits budgets pour le Salon 46 à Aigues Mortes"
+        title:"L'Entracte",
+        attachment:"image/website/creation-site-aigues-mortes-entracte.png",
+        object:"Snack sur Aigues-Mortes",
+        link:"https://chaplin16.github.io/entracte/",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour l entracte à Aigues Mortes"
     },
     {
         id:5,
-        title:"Judo Club Aigues-Mortais",
-        attachment:"image/website/creation-site-aigues-mortes-judo.png",
-        object:"Club de Judo",
-        link:"https://judo-aiguesmortais.fr",
-        code:"Développé avec Word Press",
-        alt:"creation site internet rapide petits budgets pour le judo Aigues Mortais"
-    },
-    {
-        id:6,
-        title:"Restaurant Punjab",
-        attachment:"image/website/creation-site-aigues-mortes-cordonnerie-punjab.png",
-        object:"Restaurant Indien à Sète",
-        link:"https://restaurant-indien-sete.jimdofree.com/",
-        code: "Développé avec avec JIMDO",
-        alt:"creation site internet rapide petits budgets pour le restaurant PUNJAB"
-    },
-    {
-        id:7,
-        title:"Aigues-Bike",
-        attachment:"image/website/creation-site-aigues-mortes-aigues-bike.png",
-        object:"Location de vélo et trottinettes",
-        link:"https://chaplin16.github.io/aigues_bike/",
-        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour Aigues Bike"
-    },
-    {
-        id:8,
         title:"Le Carrousel",
         attachment:"image/website/creation-site-internet-aigues-mortes-carrousel.png",
         object:"Magnifique manège de chevaux en bois",
         link:"https://carrousel-aiguesmortes.fr",
         code:"Développé avec Word Press",
         alt:"creation site internet rapide petits budgets pour le carrousel a Aigues Mortes"
+    },
+    {
+        id:6,
+        title:"Les Artisans de Samaël",
+        attachment:"image/website/creation-site-internet-aigues-mortes-artisans-samel.png",
+        object:"Boutique de santons de Provence",
+        link:"https://www.artisansdesamael.com",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour les artisans de samael à Aigues Mortes"
+    },
+    {
+        id:7,
+        title:"Projet perso d'un blog",
+        attachment:"image/website/creation-site-aigues-mortes-projet-perso-blog-next-react.png",
+        object:"Projet perso ✨",
+        link:"https://projet2-fictif-next.vercel.app/",
+        code: "Codé avec Next.js avec une API externe",
+        alt:"creation d'un blog en next avec une Api externe"
+    },
+    {
+        id:8,
+        title:"Salon 46",
+        attachment:"image/website/creation-site-aigues-mortes-salon-46.png",
+        object:"Salon de coiffure sur Aigues-Mortes",
+        link:"https://salon-46.fr",
+        code:"Développé avec Word Press",
+        alt:"creation site internet rapide petits budgets pour le Salon 46 à Aigues Mortes"
     },
     {
         id:9,
@@ -82,87 +82,24 @@ const website = [
     },
     {
         id:10,
-        title:"Cordonnerie des Remparts",
-        attachment:"image/website/creation-site-internet-aigues-mortes-cordonnerie-remparts.png",
-        object:"Cordonnerie sur Aigues-Mortes",
-        link:"https://chaplin16.github.io/cordonnerie/",
-        code: "Codé en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour le cordonnier à Aigues Mortes"
+        title:"Restaurant Punjab",
+        attachment:"image/website/creation-site-aigues-mortes-cordonnerie-punjab.png",
+        object:"Restaurant Indien à Sète",
+        link:"https://restaurant-indien-sete.jimdofree.com/",
+        code: "Développé avec avec JIMDO",
+        alt:"creation site internet rapide petits budgets pour le restaurant PUNJAB"
     },
     {
         id:11,
-        title:"JoTraiteur",
-        attachment:"image/website/creation-site-aigues-mortes-jo-traiteur.png",
-        object:"Traiteur pour les grands événements",
-        link:"https://jotraiteur.fr",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide  petits budgets pour jo traiteur à Aigues Mortes"
+        title:"Judo Club Aigues-Mortais",
+        attachment:"image/website/creation-site-aigues-mortes-judo.png",
+        object:"Club de Judo",
+        link:"https://judo-aiguesmortais.fr",
+        code:"Développé avec Word Press",
+        alt:"creation site internet rapide petits budgets pour le judo Aigues Mortais"
     },
     {
         id:12,
-        title:"Jpcapsules",
-        attachment:"image/website/creation-site-aigues-mortes-jpcapsules.png",
-        object:"Site de ventes de capsules de champagne",
-        link:"https://jpcapsules.fr",
-        code: "Développé avec Prestashop",
-        alt:"creation site internet rapide petits budgets pour jpcapsules à Aigues Mortes"
-    },
-    {
-        id:13,
-        title:"L'Entracte",
-        attachment:"image/website/creation-site-aigues-mortes-entracte.png",
-        object:"Snack sur Aigues-Mortes",
-        link:"https://chaplin16.github.io/entracte/",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour l entracte à Aigues Mortes"
-    },
-    {
-        id:14,
-        title:"Les Artisans de Samaël",
-        attachment:"image/website/creation-site-internet-aigues-mortes-artisans-samel.png",
-        object:"Boutique de santons de Provence",
-        link:"https://www.artisansdesamael.com",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour les artisans de samael à Aigues Mortes"
-    },
-    {
-        id:15,
-        title:"Appartement à louer",
-        attachment:"image/website/creation-site-aigues-mortes-location-camargue.png",
-        object:"Appartement à louer sur Aigues-Mortes",
-        link:"https://locationappartementaiguesmortes.fr",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour la location appartement à Aigues Mortes"
-    },
-    {
-        id:16,
-        title:"NokIsland",
-        attachment:"image/website/creation-site-aigues-mortes-nokisland.png",
-        object:"Boutique de vêtements sur Aigues-Mortes",
-        link:"https://chaplin16.github.io/nokIsland/",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour nokisland à Aigues Mortes"
-    },
-    {
-        id:17,
-        title:"Nuit de Folie",
-        attachment:"image/website/creation-site-internet-aigues-mortes-nuit-de-folie.png",
-        object:"Boutique de sous-vêtements féminins",
-        link:"https://nuitdefolie.fr",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour nuit de folie à Aigues Mortes"
-    },
-    {
-        id:18,
-        title:"O Phil des Saveurs",
-        attachment:"image/website/creation-site-internet-aigues-mortes-o-phil-des-saveurs.png",
-        object:"Traiteur sur les marchés",
-        link:"https://ophildessaveurs.fr",
-        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
-        alt:"creation site internet rapide petits budgets pour o phil des saveursà Aigues Mortes"
-    },
-    {
-        id:19,
         title:"Artiste Roudil",
         attachment:"image/website/creation-site-internet-aigues-mortes-roudil.png",
         object:"Peintre, sculpteur, ROUDIL Bruno",
@@ -171,7 +108,88 @@ const website = [
         alt:"creation site internet rapide petits budgets pour l artiste roudil à Aigues Mortes"
     },
     {
+        id:13,
+        title:"Les Fashion du Sud",
+        attachment:"image/website/creation-site-aigues-mortes-fashion-du-sud.png",
+        object:"Boutique fashion de vêtements pour femme",
+        link:"https://lesfashiondusud.fr",
+        code:"Développé avec Word Press",
+        alt:"creation site internet rapide petits budgets pour les fashion du sud à Aigues-Mortes."
+    },
+    {
+        id:14,
+        title:"Appartement à louer",
+        attachment:"image/website/creation-site-aigues-mortes-location-camargue.png",
+        object:"Appartement à louer sur Aigues-Mortes",
+        link:"https://locationappartementaiguesmortes.fr",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour la location appartement à Aigues Mortes"
+    },
+    {
+        id:15,
+        title:"Angélina Bike",
+        attachment:"image/website/creation-site-aigues-mortes-angelina-bike.png",
+        object:"Boutique de location et d'achat de vélo électrique et moto",
+        link:"https://angelina-bike.fr",
+        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets  pour Angélina à la Grande Motte."
+    },
+    {
+        id:16,
+        title:"David BARRY",
+        attachment:"image/website/creation-site-aigues-mortes-cordonnerie-david-barry.png",
+        object:"Site pour un maçon",
+        link:"https://david-barry.fr",
+        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour le site du maçon d Aigues Mortes"
+    },
+    {
+        id:17,
+        title:"Aigues-Bike",
+        attachment:"image/website/creation-site-aigues-mortes-aigues-bike.png",
+        object:"Location de vélo et trottinettes",
+        link:"https://chaplin16.github.io/aigues_bike/",
+        code:"Codé en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour Aigues Bike"
+    },
+    {
+        id:18,
+        title:"JoTraiteur",
+        attachment:"image/website/creation-site-aigues-mortes-jo-traiteur.png",
+        object:"Traiteur pour les grands événements",
+        link:"https://jotraiteur.fr",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide  petits budgets pour jo traiteur à Aigues Mortes"
+    },
+    {
+        id:19,
+        title:"NokIsland",
+        attachment:"image/website/creation-site-aigues-mortes-nokisland.png",
+        object:"Boutique de vêtements sur Aigues-Mortes",
+        link:"https://chaplin16.github.io/nokIsland/",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour nokisland à Aigues Mortes"
+    },
+    {
         id:20,
+        title:"Nuit de Folie",
+        attachment:"image/website/creation-site-internet-aigues-mortes-nuit-de-folie.png",
+        object:"Boutique de sous-vêtements féminins",
+        link:"https://nuitdefolie.fr",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour nuit de folie à Aigues Mortes"
+    },
+    {
+        id:21,
+        title:"O Phil des Saveurs",
+        attachment:"image/website/creation-site-internet-aigues-mortes-o-phil-des-saveurs.png",
+        object:"Traiteur sur les marchés",
+        link:"https://ophildessaveurs.fr",
+        code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
+        alt:"creation site internet rapide petits budgets pour o phil des saveursà Aigues Mortes"
+    },
+    {
+        id:22,
         title:"Boutique Scénario",
         attachment:"image/website/creation-site-internet-aigues-mortes-scenario.png",
         object:"Vente de vêtements pour femme",
@@ -180,10 +198,10 @@ const website = [
         alt:"creation site internet rapide petits budgets pour Scenario à Aigues Mortes"
     },
     {
-        id:21,
+        id:23,
         title:"Verre Azur",
         attachment:"image/website/creation-site-internet-aigues-mortes-verre-azur.png",
-        object:"Création de bijoux",
+        object:"Créatice de bijoux en verre",
         link:"https://verre-azur.com",
         code:"Codé  en Html, Css, Javascript, Bootstrap, Sass",
         alt:"creation site internet rapide petits budgets pour verre azur à Aigues Mortes"
